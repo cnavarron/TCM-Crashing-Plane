8e07e682f411f93e7d5d838ea9ab1e90bccc401b
Charlie <cnavarron@edu.tecnocampus.cat>
2025-04-02 15:47:24 +0200

    Listo

--- Assets/Scripts/NewMonoBehaviourScript.cs
+++ Assets/Scripts/NewMonoBehaviourScript.cs
@@ -1,12 +1,11 @@
 using UnityEngine;
 
-public class ConsoleLog : MonoBehaviour
+public class NewMonoBehaviourScript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
-
     }
 
     // Update is called once per frame
